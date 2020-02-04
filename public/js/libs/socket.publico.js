f6989c2bab6c22c2ae4d58e7c3b2f8b6d6496c4e
Alejandro Exequiel Hernández Lara <Alejandrehl@icloud.com>
2020-02-04 14:53:22 -0300

    Sonido al clickear siguiente ticket

--- public/js/libs/socket.publico.js
+++ public/js/libs/socket.publico.js
@@ -20,7 +20,7 @@ const lblEscritorios = [
 
 socket.on("estadoActual", data => actualizaHTML(data.ultimos4));
 socket.on("ultimos4", data => {
-  const audio = new Audio("audio/new-ticket.mp3");
+  var audio = new Audio("audio/new-ticket.mp3");
   audio.play();
 
   actualizaHTML(data.ultimos4);
