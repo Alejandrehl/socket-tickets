beb63634c31af6e4e1bf3eb46e574c77d5524bf7
Alejandro Exequiel Hernández Lara <Alejandrehl@icloud.com>
2020-02-04 10:41:11 -0300

    colección de tickets pendientes

--- server/classes/ticket-control.js
+++ server/classes/ticket-control.js
@@ -1,15 +1,24 @@
 const fs = require("fs");
 
+class Ticket {
+  constructor(numero, escritorio) {
+    this.numero = numero;
+    this.escritorio = escritorio;
+  }
+}
+
 class TicketControl {
   constructor() {
     this.ultimo = 0;
     this.hoy = new Date().getDate();
+    this.tickets = [];
 
     let data = require("../data/data.json");
     const { ultimo, hoy } = data;
 
     if (hoy === this.hoy) {
       this.ultimo = ultimo;
+      this.ticket = data.tickets;
     } else {
       this.reiniciarConteo();
     }
@@ -17,11 +26,14 @@ class TicketControl {
 
   reiniciarConteo = () => {
     this.ultimo = 0;
+    this.ticket = [];
     this.grabarArchivo();
   };
 
   siguiente = () => {
     this.ultimo += 1;
+    let ticket = new Ticket(this.ultimo, null);
+    this.tickets.push(ticket);
     this.grabarArchivo();
 
     return `Ticket ${this.ultimo}`;
@@ -34,7 +46,8 @@ class TicketControl {
   grabarArchivo = () => {
     let jsonData = {
       ultimo: this.ultimo,
-      hoy: this.hoy
+      hoy: this.hoy,
+      tickets: this.tickets
     };
 
     let jsonDataString = JSON.stringify(jsonData);
